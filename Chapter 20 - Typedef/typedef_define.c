ca62f85bfe82bf588ba5a9f37767b7624551b936
krambo23 <taj.keshav@gmail.com>
2021-11-19 00:43:22 +0000

    Chapter 20

---
+++ Chapter 20 - Typedef/typedef_define.c
@@ -0,0 +1,14 @@
+// pg 137 , 138
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+int main()
+{
+	printf("The value of TRUE : %d\n", TRUE);
+	printf("The value of FALSE : %d\n", FALSE);
+
+	return 0;
+}
