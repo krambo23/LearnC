606005e9e68b2721dec31800fa3af8c3b5fecd83
krambo23 <taj.keshav@gmail.com>
2021-11-19 00:49:28 +0000

    Chapter 30

---
+++ Chapter 30 - Command Line Arguments/argument_2.c
@@ -0,0 +1,19 @@
+// pg 175 , 176
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Program name is : %s\n", argv[0]);
+
+	if (argc == 2)
+	{
+		printf("The argument supplied is : %s\n", argv[1]);
+	}
+	else if (argc > 2)
+	{
+		printf("Too many arguments supplied.\n");
+	}
+
+	return 0;
+}
