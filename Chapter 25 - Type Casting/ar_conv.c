57159783f124884cfd8aac09c15dce9a4c1c3cab
krambo23 <taj.keshav@gmail.com>
2021-11-19 00:46:16 +0000

    Chapter 25

---
+++ Chapter 25 - Type Casting/ar_conv.c
@@ -0,0 +1,15 @@
+// pg 158
+
+#include <stdio.h>
+
+int main()
+{
+	int i = 17;
+	char c = 'c'; // ascii value = 99
+	float sum;
+
+	sum = i + c;
+	printf("Value of Sum : %f\n", sum);
+
+	return 0;
+}
---
+++ Chapter 25 - Type Casting/int_promotion.c
@@ -0,0 +1,15 @@
+// pg 157
+
+#include <stdio.h>
+
+int main()
+{
+	int i = 17;
+	char c = 'c'; // ascii value = 99
+	int sum;
+
+	sum = i + c;
+	printf("Value of Sum : %d\n", sum);
+
+	return 0;
+}
