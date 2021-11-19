4881c48600d23e5f1f15ba64646fbef58e7a8bc5
krambo23 <taj.keshav@gmail.com>
2021-11-19 00:32:00 +0000

    Chapter 7

---
+++ Chapter 7 - Constants and Literals/Character_Constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() 
+{
+	printf("Hello\tWorld\n\n");
+
+	return 0;
+}
+
+//pg21
---
+++ Chapter 7 - Constants and Literals/Defining_Constants_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() 
+{
+	const int LENGTH = 10;
+	const int WIDTH = 5;
+	const char NEWLINE = '\n';
+	int area;
+
+	area = LENGTH * WIDTH;
+	printf("Value of Area = %d", area);
+	printf("%c", NEWLINE);
+
+	return 0;
+}
+
+//pg 23
