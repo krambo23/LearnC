a9490e357a6b7dec8e078776ec2b653fa9c12cbd
krambo23 <taj.keshav@gmail.com>
2021-11-19 00:38:42 +0000

    Chapter 16

---
+++ Chapter 16 - Strings/string.c
@@ -0,0 +1,11 @@
+//pg 117 , 118
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+	printf("Greeting message is : %s\n", greeting);
+
+	return 0;
+}
